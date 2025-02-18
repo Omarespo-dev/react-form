0040b4d88f09986d5609cf986090a011d329abd9
Omarespo-dev <omaresposito21@gmail.com>
2025-02-18 16:27:35 +0100

    AGGIUNTA ELEMENTI NELLA LISTA ORA SETTO IL BUTTON PER RIMUOVERE

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -16,7 +16,8 @@ export default function Main() {
     // articles Contiene l'array di articoli.------ setArticles È una funzione che permette di aggiornare lo stato. ---- useState() Gestisce lo stato
     const [listTitle, setListTitle] = useState(articles)
 
-
+    // Nuovo stato con stringa vuota dove si andranno ad aggiungere gli elementi che inserira l utente
+    const [newlistTitle, newSetListTitle] = useState("")
 
 
     // 1) Aggiungo input di tipo text con il valore che corrispodnde a listTitle e gli metto onChange → È un evento di React che si attiva ogni volta che il valore di un campo <input> cambia (ad esempio, quando l’utente digita qualcosa).
@@ -28,24 +29,34 @@ export default function Main() {
         // no fa ricaricare la pagina all invio 
         event.preventDefault();
 
-        console.log('Il nome inviato è:' + listTitle)
+        // Facciamo la copia dell array di stringhe 
+        const copiaLista= [...listTitle, newlistTitle];
+        // e aggiorno anche lo stato
+        setListTitle(copiaLista)
+
+        // Test per vedere se stampa qualsiasi cosa nell campo form
+        console.log('Il nome inviato è:' + copiaLista)
     }
 
+    
     // 3) Ho fatto  un array di stringhe dove lo vado a mettere nel <ul> facendo il map --- Poi aggiungo il button elimina a li
+
+    // 4) Abbiamo questo array di stringhe ma vogliamo che al click del button INVIA aggiunge una altra lista compilata dal input
+    // -Per farlo bisogna creare uno useState vuoto dove all interno andranno gli elementi che aggiunge l utente
     
     return (<>
         {/* PARTE GESTIONE DEL FORM */}
 
         <form onSubmit={handleSubmit}>
             <input type="text"
-                onChange={e => { setListTitle(e.target.value) }}
+                onChange={e => { newSetListTitle(e.target.value) }}
             />
             <button>INVIA</button>
         </form>
         
         {/* PARTE GESTIONE DELLA LISTA  */}
         <ul>
-            {articles.map((article, i) => <li key={i}>{article}
+            {listTitle.map((list, i) => <li key={i}>{list}
             <button>-</button>
             </li>)}
             
